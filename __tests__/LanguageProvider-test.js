88ece85b3589c1915a408b0c139f3414ba19fd52
Exorth <nico.caill@live.fr>
2019-10-11 08:28:15 -0700

    Implementing Jest and Enzyme

--- __tests__/LanguageProvider-test.js
+++ __tests__/LanguageProvider-test.js
@@ -1,19 +1,44 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import {View} from 'react-native'
 import React from 'react';
-import {View} from 'react-native';
+import { shallow } from 'enzyme';
 import {LanguageProvider, TransText} from '../src/LanguageProvider';
 
+// Note: this is just for use with Jest snapshot testing
+// and comes packaged with react-native init project.
+// You do not need this if using Enzyme 'toMatchSnapshot' etc.
 import renderer from 'react-test-renderer';
 
-test('renders correctly with language and value', () => {
+// This test just uses Jest snapshot testing
+/*it('renders correctly, test using Jest', () => {
+  renderer.create(
+    <LanguageProvider language={"en-US"}>
+      <View>
+          <TransText dictionary = {{"en-US" : "Hello {name} !","fr-FR" : "Salut {name} !"}} values = {{"name":"Joe"}}/>
+      </View>
+    </LanguageProvider>);
+});*/
 
 
-  const translation = renderer.create(
-        <LanguageProvider language={"en-US"}>
-            <View>
-                <TransText dictionary = {{"en-US" : "Hello {name} !","fr-FR" : "Salut {name} !"}} values = {{"name":"Joe"}}/>
-            </View>
-        </LanguageProvider>
-    
-    ).toJSON();
-  expect(tree).toMatchSnapshot();
+const name = "Joe"
+const dic = {
+  "en-US" : "Hello {name} !",
+  "fr-FR" : "Salut {name} !"
+}
+
+// Using Jest + Enzyme
+describe('<LanguageProvider/>', () => {
+  it('renders correctly, test using Jest + Enzyme', () => {
+    expect(shallow(
+      <LanguageProvider language={"en-US"}>
+        <View>
+            <TransText dictionary = {dic} values = {{"name":name}}/>
+        </View>
+      </LanguageProvider>
+    )).toMatchSnapshot();
+  });
 });
