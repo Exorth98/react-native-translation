c91ef774ceb3e5f8fd70ecfdf2ec257656668dac
Nicolas Caillieux <nico.caill@live.fr>
2019-10-15 12:29:36 -0700

    Updating index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,25 @@
 import { LanguageProvider, TransText, AnimatedTransText } from './LanguageProvider';
 import React from 'react';
 
-interface LanguageProviderProps {
+type Dictionary = Object | string
+
+interface ILanguageProviderProps {
     defaultLanguage?: string;
+    translations?: Object;
     language: string;
 }
 
-interface TransTextProps {
-    dictionary: Object;
+interface ITransTextProps {
+    dictionary: Dictionary;
     values?: Object;
 }
 
-interface AnimatedTransTextProps {
-    dictionary: Object;
+interface IAnimatedTransTextProps {
+    dictionary: Dictionary;
     values?: Object;
 }
 
-export const LanguageProvider: React.SFC<LanguageProviderProps>;
-export const TransText: React.SFC<TransTextProps>;
-export const AnimatedTransText: React.SFC<AnimatedTransTextProps>;
+export const LanguageProvider: React.SFC<ILanguageProviderProps>;
+export const TransText: React.SFC<ITransTextProps>;
+export const AnimatedTransText: React.SFC<IAnimatedTransTextProps>;
 
